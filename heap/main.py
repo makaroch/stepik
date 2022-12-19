1be17153e6fa66bc3799567eb2e84bed33d7207f
Alex <da.ek.ned@yandex.ru>
2022-12-19 13:38:46 +0300

    ну теперь точно правильно работает добавление эементов + функция прохода по листьям

--- heap/main.py
+++ heap/main.py
@@ -3,6 +3,8 @@ def input_my():
     return input('задайте  операцию\n').split()
 
 def Svap(num1, num2):
+    if num1 == -1 or num2 == -1: # из за касяка с индексом может приодить -1 из-за этого меняем местами не те элементы
+        return                   # соот ветственно если пришла -1 ничего не нужно менять местами
     heap[num1], heap[num2] = heap[num2], heap[num1]
 
 def Insert(num):
@@ -13,11 +15,20 @@ def Insert(num):
         if heap[1] < heap[0]: Svap(0, 1)
     else:
         i = len(heap) - 1
-        while heap[i] < heap[ (i-1)//2 ]:
-            Svap(i, ((i-1)//2))
+        while heap[i] < heap[((i-1)//2)]:
+            Svap(i, ((i-1)//2) )
             i = (i-1)//2
-        # if heap[-1] < heap[(len(heap) - 1) // 2]:   (i-1)//2
-        #     svap(heap[-1], heap[(len(heap) - 1) // 2])
+def heap_max_index(heap_list: list):
+    i = len(heap_list) - 1
+    max_index = i
+    count_operant = 0
+    while (2*i + 1) > (len(heap_list) - 1):
+        if heap_list[max_index] < heap_list[i]: max_index = i
+        i -= 1
+        count_operant +=1
+    print(count_operant)
+    return max_index
+
 
 def control():
     count_operations = int(input('кол-во операций\n'))
@@ -25,9 +36,11 @@ def control():
     for _ in range(count_operations):
         str_list = input_my()
         if str_list[0] == 'ExtractMax':
-            print(max(heap))
+            print(heap[heap_max_index(heap)])
         else:
             Insert(int(str_list[1]))
             print(heap)
 
 control()
+
+
